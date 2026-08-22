368a3fd962bcb1f1c21859de86154146e262a601
sharan baral <saranbrl35@gmail.com>
2026-08-22 09:53:45 +0545

    seo: gtag added

--- app/layout.js
+++ app/layout.js
@@ -52,7 +52,29 @@ export default function RootLayout({ children }) {
       className={`${inter.variable} ${playfair.variable} ${sourceSerif.variable} ${jetbrainsMono.variable} h-full antialiased`}
       suppressHydrationWarning
     >
+      <head>
+        <meta name="google-site-verification" content="kWagdBbHfn_oEo8h_rwaKFu5V7e9dtWMwOU9W9VaUfY" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KTM26DLW');
+            `,
+          }}
+        />
+      </head>
       <body className="min-h-full flex flex-col font-sans" suppressHydrationWarning>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KTM26DLW"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <AuthProvider>
           {children}
         </AuthProvider>
